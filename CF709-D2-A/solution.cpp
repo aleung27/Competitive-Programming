5d93fdf1fed26f06b9a6e55e0e3b7daa2dff9ca4
adam <adamjleung123@gmail.com>
2020-06-25 12:54:59 +1000

    CF709-D2-A

--- CF709-D2-A/solution.cpp
+++ CF709-D2-A/solution.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 int main () {
-    int noranges, maxsize, limit;
-    scanf("%d %d %d", &noranges, &maxsize, &limit);
+	int noranges, maxsize, limit;
+	scanf("%d %d %d", &noranges, &maxsize, &limit);
 
-    int curr = 0;
-    int count = 0;
+	int curr = 0;
+	int count = 0;
 
-    int orange;
-    for(int i = 0; i < noranges; i++) {
-        scanf("%d", &orange);
-        if (orange > maxsize) continue;
+	int orange;
+	for(int i = 0; i < noranges; i++) {
+	    scanf("%d", &orange);
+	    if (orange > maxsize) continue;
 
-        curr += orange;
-        if (curr > limit){
-            count++;
-            curr = 0;
-        } 
-            
-    }    
+	    curr += orange;
+	    if (curr > limit){
+		count++;
+		curr = 0;
+	    } 
+		
+	}    
 
-    printf("%d\n", count);
+	printf("%d\n", count);
 
 
-    return 0;
+	return 0;
 }

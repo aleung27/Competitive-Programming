7355af4125b0d90e0907afb2f9418d48b11fb2a8
adam <adamjleung123@gmail.com>
2020-06-25 13:07:55 +1000

    CF799-D2-A

--- CF799-D2-A/solution.cpp
+++ CF799-D2-A/solution.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main () {
-    
+    int ncakes, time, batch, newoventime;
+    scanf("%d %d %d %d", &ncakes, &time, &batch, &newoventime);
+
+    int totaltime = time+newoventime;
+    totaltime = totaltime/time;
+    int totalcakes = totaltime*batch;
+
+    totalcakes < ncakes ? printf("YES\n") : printf("NO\n");
 
     return 0;
 }

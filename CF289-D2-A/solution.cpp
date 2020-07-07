23f313dd874a0feeadcc4eea328c352a9f881e88
adam <adamjleung123@gmail.com>
2020-07-07 12:11:45 +1000

    CF289-D2-A & CF699-D2-A

---
+++ CF289-D2-A/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a)*b).Y
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+
+int main () {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int total = 0;
+    int a, b;
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d", &a, &b);
+        total += b-a+1;
+    }
+
+    printf("%d\n", (k - (total % k)) % k);
+
+    return 0;
+}

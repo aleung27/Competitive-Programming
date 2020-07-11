a04a88f9701f3c0934432a28df30da51f65685bc
adam <adamjleung123@gmail.com>
2020-07-11 13:30:18 +1000

    CF463-D2-B

---
+++ CF463-D2-B/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a)*b).Y
+#define FASTIO ios::sync_with_stdio(false);
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+
+int main () {
+    int n;
+    scanf("%d", &n);
+
+    int max = INT_MIN;
+    int next;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &next);
+        if (max < next) max = next;
+    }
+
+    printf("%d\n", max);
+
+    return 0;
+}

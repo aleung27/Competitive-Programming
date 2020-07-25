0b9dda3dd4c5583202e6ce9dc4009c05d90e1b2f
adam <adamjleung123@gmail.com>
2020-07-25 14:54:56 +1000

    CF520-D2-B

---
+++ CF520-D2-B/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a)*b).Y
+#define FASTIO ios::sync_with_stdio(false);
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+
+int main () {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int cnt = 0;
+    while(b != a) {
+        if(b > a && !(b % 2)) b /= 2;
+        else b++;
+        cnt++; 
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}

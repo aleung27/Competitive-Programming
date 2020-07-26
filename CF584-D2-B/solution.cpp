a8fddab3aee97f49a0ee1c1a851e315a0598a04f
adam <adamjleung123@gmail.com>
2020-07-26 15:18:29 +1000

    CF584-D2-B

---
+++ CF584-D2-B/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a)*(b)).Y
+#define DOTPROD(a, b) ((a).X*(b).X + (a).Y+(b).Y)
+#define FASTIO ios::sync_with_stdio(false);
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+
+template <class T>
+T modpow(T x, T n, T m) {
+    if(n == 0) return 1 % m;
+    ll u = modpow(x, n/2, m);
+    u = (u*u)%m;
+    if(n%2 == 1) u = (u*x)%m;
+    return u;
+}
+
+#define MOD 1000000007
+
+int main () {
+    int n;
+    scanf("%d", &n);
+
+    ll poss = modpow(27, n, MOD);
+    ll subtract = modpow(7, n, MOD);
+
+    ll total = (poss - subtract) % MOD;
+    total < 0 ? printf("%lld\n", total + MOD) : printf("%lld\n", total); 
+
+    return 0;
+}

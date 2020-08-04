9bee0eabc1f2c04cdf7151349a873e7113953eb7
adam <adamjleung123@gmail.com>
2020-08-04 12:45:00 +1000

    CF1009-D2-B

---
+++ CF1009-D2-B/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a)*(b)).Y
+#define DOTPROD(a, b) ((a).X*(b).X + (a).Y+(b).Y)
+#define FASTIO ios::sync_with_stdio(false);
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+
+template <class T>
+T modpow(T x, T n, T m) {
+    if(n == 0) return 1 % m;
+    ll u = modpow(x, n/2, m);
+    u = (u*u)%m;
+    if(n%2 == 1) u = (u*x)%m;
+    return u;
+}
+
+int main () {
+    FASTIO
+    string s;
+    string ans = "";
+    string ones = "";
+    cin >> s;
+
+    for(const auto& c: s) {
+        if(c == '1') ones += "1";
+        else ans += c;
+    }
+
+    int index = ans.find("2");
+    cout << ans.substr(0, index) << ones << (index == string::npos ?  "": ans.substr(index, ans.length())) << endl;
+
+    return 0;
+}

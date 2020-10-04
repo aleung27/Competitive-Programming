21f8699cef838f7bca635dd94ab8ec42ddcab27d
adam <adamjleung123@gmail.com>
2020-10-04 20:13:34 +1100

    UVA-10509

---
+++ UVA-10509/solution.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a) * (b)).Y
+#define DOTPROD(a, b) ((a).X * (b).X + (a).Y + (b).Y)
+#define FASTIO ios::sync_with_stdio(false);
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+template <class T>
+T lcm(T a, T b) {
+    return a * b / gcd<T>(a, b);
+}
+
+template <class T>
+T modpow(T x, T n, T m) {
+    if (n == 0) return 1 % m;
+    ll u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1) u = (u * x) % m;
+    return u;
+}
+
+int main() {
+    FASTIO
+    double n;
+    cout << fixed;
+    cout << setprecision(4);
+
+    while(cin >> n) {
+        if(!n) break;
+        
+        int a = cbrt(n);
+        double dx = (n-a*a*a)/(3*a*a);
+        cout << a + dx << endl;
+    }
+
+    return 0;
+}

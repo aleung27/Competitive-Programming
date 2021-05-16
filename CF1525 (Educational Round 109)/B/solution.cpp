af907d64ba83323195c118ba076ae7dc0df3cc2d
adam <adamjleung123@gmail.com>
2021-05-16 19:44:37 +1000

    CF1525 (Educational Round 109)

---
+++ CF1525 (Educational Round 109)/B/solution.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a) * (b)).Y
+#define DOTPROD(a, b) ((a).X * (b).X + (a).Y + (b).Y)
+#define FASTIO ios::sync_with_stdio(false);
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+template <class T>
+T lcm(T a, T b) {
+    return a * b / gcd<T>(a, b);
+}
+
+template <class T>
+T modpow(T x, T n, T m) {
+    if (n == 0) return 1 % m;
+    ll u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1) u = (u * x) % m;
+    return u;
+}
+
+int main() {
+    FASTIO
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> vals;
+        int next;
+        bool sorted = true;
+        for (int i = 0; i < n; i++) {
+            cin >> next;
+            vals.push_back(next);
+            if (next != i + 1) sorted = false;
+        }
+
+        if (sorted)
+            cout << 0 << endl;
+        else if (vals[0] == 1 || vals[n - 1] == n)
+            cout << 1 << endl;
+        else if (vals[0] == n && vals[n - 1] == 1)
+            cout << 3 << endl;
+        else
+            cout << 2 << endl;
+    }
+
+    return 0;
+}

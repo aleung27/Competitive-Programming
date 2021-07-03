0793b125a1b6f65503e01584360e428809bf755e
adam <adamjleung123@gmail.com>
2021-07-04 00:45:06 +1000

    CF1542 (Contest 729)

---
+++ CF1542 (Contest 729)/B/solution.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define X real()
+#define Y imag()
+#define EPS 0.000000001
+#define M_PI 3.14159265358979323846
+#define CROSSPROD(a, b) (conj(a) * (b)).Y
+#define DOTPROD(a, b) ((a).X * (b).X + (a).Y + (b).Y)
+#define FASTIO ios::sync_with_stdio(false);
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+template <class T>
+T gcd(T a, T b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+template <class T>
+T lcm(T a, T b) {
+    return a * b / gcd<T>(a, b);
+}
+
+template <class T>
+T modpow(T x, T n, T m) {
+    if (n == 0) return 1 % m;
+    ll u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1) u = (u * x) % m;
+    return u;
+}
+
+int main() {
+    FASTIO
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        bool done = false;
+
+        for (int i = 0; i <= 30; i++) {
+            ll power = pow(a, i);
+            if (n - power < 0) break;
+            if ((n - power) % b == 0) {
+                done = true;
+                break;
+            }
+        }
+
+        done ? cout << "Yes" << endl : cout << "No" << endl;
+    }
+
+    return 0;
+}
